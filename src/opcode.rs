e12afdc4c1c9b0bb7fc4c6f0249468b0265c1ff6
Berzan <berzan@tutanota.com>
2023-10-19 14:02:21 +0000

    define: `Opcode` enum

---
+++ src/opcode.rs
@@ -0,0 +1,34 @@
+/// An enum that represents opcode type for the virtual machine.
+#[derive(Clone, Copy)]
+#[repr(u8)]
+pub enum Opcode {
+    INVALID = 0,
+    PUSH,
+    POP,
+    STORE,
+    LOAD,
+    ADD,
+    RET,
+}
+
+impl From<u8> for Opcode {
+    /// Converts to a `Opcode` from `u8` value.
+    fn from(value: u8) -> Self {
+        match value {
+            1 => Self::PUSH,
+            2 => Self::POP,
+            3 => Self::STORE,
+            4 => Self::LOAD,
+            5 => Self::ADD,
+            6 => Self::RET,
+            _ => Self::INVALID,
+        }
+    }
+}
+
+impl From<Opcode> for u8 {
+    /// Converts to a `u8` from `Opcode` value.
+    fn from(value: Opcode) -> Self {
+        value as u8
+    }
+}
