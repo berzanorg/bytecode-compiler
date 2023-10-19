54d43d491336558cf09e0bb792eb40e7338ae47a
Berzan <berzan@tutanota.com>
2023-10-19 18:36:37 +0000

    define: `ParseError` enum

--- src/error.rs
+++ src/error.rs
@@ -22,3 +22,42 @@ impl Display for VmError {
         write!(f, "RUNTIME ERROR: {}", msg)
     }
 }
+
+#[derive(Debug)]
+pub enum ParseError<'a> {
+    MistakenOpcode(&'a str),
+    OpcodeRequired(&'a str),
+    ValueRequired(&'a str),
+    IndexRequired(&'a str),
+    MistakenValue(&'a str),
+    MistakenIndex(&'a str),
+}
+
+impl<'a> Display for ParseError<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::MistakenOpcode(mistaken_opcode) => write!(
+                f,
+                "PARSING ERROR: `{mistaken_opcode}` is not a valid opcode"
+            ),
+            ParseError::OpcodeRequired(number_string) => write!(
+                f,
+                "PARSING ERROR: an opcode is required before `{number_string}`"
+            ),
+            ParseError::ValueRequired(opcode_string) => write!(
+                f,
+                "PARSING ERROR: a value is required after `{opcode_string}`"
+            ),
+            ParseError::IndexRequired(opcode_string) => write!(
+                f,
+                "PARSING ERROR: an index is required after `{opcode_string}`"
+            ),
+            ParseError::MistakenValue(value_string) => {
+                write!(f, "PARSING ERROR: `{value_string}` is not a valid value")
+            }
+            ParseError::MistakenIndex(index_string) => {
+                write!(f, "PARSING ERROR: `{index_string}` is not a valid index")
+            }
+        }
+    }
+}
