80719dec60c274ad5cd08464227bda7982409980
Berzan <berzan@tutanota.com>
2023-10-19 18:13:40 +0000

    remove: `Invalid` variant

--- src/opcode.rs
+++ src/opcode.rs
@@ -1,8 +1,9 @@
+use crate::error::VmError;
+
 /// An enum that represents opcode type for the virtual machine.
 #[derive(Clone, Copy)]
 #[repr(u8)]
 pub enum Opcode {
-    INVALID = 0,
     PUSH,
     POP,
     STORE,
@@ -15,21 +16,22 @@ pub enum Opcode {
     RET,
 }
 
-impl From<u8> for Opcode {
+impl TryFrom<u8> for Opcode {
     /// Converts to a `Opcode` from `u8` value.
-    fn from(value: u8) -> Self {
+    type Error = VmError;
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
         match value {
-            1 => Self::PUSH,
-            2 => Self::POP,
-            3 => Self::STORE,
-            4 => Self::LOAD,
-            5 => Self::ADD,
-            6 => Self::SUB,
-            7 => Self::MUL,
-            8 => Self::DIV,
-            9 => Self::MOD,
-            10 => Self::RET,
-            _ => Self::INVALID,
+            0 => Ok(Self::PUSH),
+            1 => Ok(Self::POP),
+            2 => Ok(Self::STORE),
+            3 => Ok(Self::LOAD),
+            4 => Ok(Self::ADD),
+            5 => Ok(Self::SUB),
+            6 => Ok(Self::MUL),
+            7 => Ok(Self::DIV),
+            8 => Ok(Self::MOD),
+            9 => Ok(Self::RET),
+            _ => Err(VmError::InvalidOpcode),
         }
     }
 }
